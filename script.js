9a0eb6628d7ba138dbc35a41c70e820250e0b4a6
Priyanshubelwal <142991870+Priyanshubelwal17@users.noreply.github.com>
2025-03-09 00:29:17 +0530

    Switched active player and reset score on rolling 1.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Selecting elements
+const player0E1 = document.querySelector('.player--0');
+const player1E1 = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
 const current0E1 = document.getElementById('current--0');
@@ -15,8 +17,9 @@ const btnhold = document.querySelector('.btn--hold');
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceE1.classList.add('hidden');
-
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling dice functionality
 btnROll.addEventListener('click', function () {
@@ -29,8 +32,15 @@ btnROll.addEventListener('click', function () {
   if (dice !== 1) {
     // Add dice to current score
     currentScore += dice;
-    current0E1.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    // current0E1.textContent = currentScore;
   } else {
     //   Add dice to current score
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0E1.classList.toggle('player--active');
+    player1E1.classList.toggle('player--active');
   }
 });
